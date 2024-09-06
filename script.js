f15468ee51716f5905a46e1df6e7e301601f096f
natalielogi <logilogi93@yandex.ru>
2024-09-06 11:53:34 +0300

    Задание 3

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ if (userInput === password) {
 } else {
     alert('Пароль введен неверно');
 }
+
+let c = 2;
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+let d = 200;
+let e = 2;
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
