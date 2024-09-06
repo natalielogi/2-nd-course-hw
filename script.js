5ebd803fdae1ad361a3287eb50bfc171da8fe09a
natalielogi <logilogi93@yandex.ru>
2024-09-06 12:09:27 +0300

    Задание 5

--- script.js
+++ script.js
@@ -20,3 +20,39 @@ if (d > 100 || e > 100) {
 } else {
     console.log('Неверно');
 }
+
+let a =2;
+let b = 3;
+
+alert(Number(a) + Number(b));
+
+let monthsNumber = 10; 
+if (monthsNumber < 1 || monthsNumber > 12) {
+    console.log('Некорректный номер месяца');
+} else {
+    switch (monthsNumber) {
+        case 12:
+        case 1:
+        case 2:
+            console.log('Зима');
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log('Весна');
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log('Лето');
+            break;
+        case 9:
+        case 10:
+        case 11:
+            console.log('Осень');
+            break;
+    
+        default:
+            console.log('Неккоректный номер месяца');
+    }
+}
