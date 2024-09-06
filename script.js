4876adbe1227e6d781e54a261c0170dbae8997e3
natalielogi <logilogi93@yandex.ru>
2024-09-06 11:42:05 +0300

    задание 1

---
+++ script.js
@@ -0,0 +1,7 @@
+let password = 'пароль';
+let userInput = prompt('Введите пароль');
+if (userInput === password) {
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неверно');
+}
