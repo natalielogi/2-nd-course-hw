e8e9db95e092cb09f254d1779a3c05c7ae23c15c
natalielogi <logilogi93@yandex.ru>
2024-09-11 12:57:39 +0300

    задание 3

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ for (let i = 0; i < 2; i++) {
     
 }
 
-//Задание 2 //
+//Задание 2 и 3//
 
-for (let a = 1; a <= 5; a++) {
-    console.log(a)
-    
+for (let a = 1; a <= 22; a++) {
+    if (a === 6) {
+        continue;
+    }
+    console.log(a);  
 }
+
