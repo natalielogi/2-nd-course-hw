38e5edd6f31d5f3d586fac77ca904052caa86739
natalielogi <logilogi93@yandex.ru>
2024-09-18 13:26:19 +0300

    Задание 2

--- task.js
+++ task.js
@@ -7,4 +7,16 @@ function minNumber(a, b) {
 console.log(minNumber(8, 4));
 console.log(minNumber(6, 6));
 
+// Задание 2// 
 
+let a = prompt('Введите число'); 
+
+function number () {
+    if (a % 2 == 0) {
+        console.log('Число четное')
+    } else {
+        console.log('Число нечетное')
+    }
+}
+
+number();
