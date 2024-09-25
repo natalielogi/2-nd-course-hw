92b3e555feb495119b5854cd26d6a02ca7199482
natalielogi <logilogi93@yandex.ru>
2024-09-25 21:22:00 +0300

    Создала игру

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 let attempts = 0;
 
-function math() {
+function guess() {
     let userGuess;
     attempts = 0;
     
@@ -26,5 +26,50 @@ function math() {
             alert(`Поздравляю! Вы угадали число ${randomNumber} за ${attempts} попыток.`);
             break; 
         }
+    }
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1; 
+}
+
+function maths() {
+    let num1 = getRandomNumber();
+    let num2 = getRandomNumber();
+    const operations = ['+', '-', '*', '/'];
+    const operation = operations[Math.floor(Math.random() * operations.length)];
+    let correctAnswer; 
+
+    switch (operation) {
+        case '+':
+            correctAnswer = num1 + num2;
+            break;
+        case '-':
+            correctAnswer = num1 - num2;
+            break;
+        case '*':
+            correctAnswer = num1 * num2;
+            break;
+        case '/':
+            correctAnswer = num1;
+            num1 = num1 * num2;
+            break;
+    }
+
+    let userAnswer = prompt(`${num1} ${operation} ${num2} = ?`)
+
+    if (userAnswer === null) {
+        alert('Игра оконена.');
+        return;
+    }
+
+    userAnswer = parseFloat(userAnswer);
+
+    if (isNaN(userAnswer)) {
+        alert('Пожалуйста, введите корректное число!');
+    } else if (userAnswer === correctAnswer) {
+        alert('Правильно!');
+    } else {
+        alert(`Неправильно. Правильный ответ ${correctAnswer}`);
     }
 }
