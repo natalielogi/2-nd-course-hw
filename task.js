fabdf5b897ffb013ce193c16ff2a4cc6be20b45d
natalielogi <logilogi93@yandex.ru>
2024-09-25 21:48:44 +0300

    Задание 4

--- task.js
+++ task.js
@@ -16,3 +16,16 @@ console.log(numbs2.indexOf(4));
 const numbs3 = [1, 3, 5, 10, 20];
 let space = numbs3.join(' ');
 console.log(space);
+
+// Задание 4
+
+let array = [];
+
+for (let a = 0; a < 3; a++) {
+    let innerArray = [];
+    for (let j = 0; j < 3; j++) {
+        innerArray.push(1);
+    }
+    array.push(innerArray);
+}
+console.log(array);
