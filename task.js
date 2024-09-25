76c2a640872de24fd35a25ff23002cd613fa7bdc
natalielogi <logilogi93@yandex.ru>
2024-09-25 21:39:03 +0300

    Задание 2

--- task.js
+++ task.js
@@ -5,3 +5,8 @@ for (let i = 0; i < numbs.length; i++) {
     if (numbs[i] == 0) break;
     console.log(numbs[i]);
 }
+
+// Задание 2 
+
+const numbs2 = [1, 5, 4, 10, 0, 3];
+console.log(numbs2.indexOf(4));
