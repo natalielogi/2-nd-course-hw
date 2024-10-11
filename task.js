e5c7726ea4a011891c2467e0c33d4df44878d21f
natalielogi <logilogi93@yandex.ru>
2024-10-11 12:26:18 +0300

    Выполнила задания

--- task.js
+++ task.js
@@ -1,80 +1,70 @@
-// Задание 1 
+// Задание 1
 
-const people = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 }
- ];
+const headingEl = document.querySelector('.heading');
+const buttonEl = document.querySelector('.button');
 
- people.sort((a, b) => a.age - b.age);
-
-console.log(people);
+buttonEl.addEventListener ('click', function() {
+    if (headingEl.style.display === 'none') {
+        headingEl.style.display = 'block';
+        buttonEl.textContent = 'Скрыть'; 
+    } else {
+        headingEl.style.display = 'none';
+        buttonEl.textContent = 'Показать';
+    }
+});
 
 // Задание 2
 
-function isPositive(num) {
-    return num > 0;
-}
+const paragraphEl = document.querySelector('.paragraph');
+const changeColorEl = document.querySelector('.change-color');
 
-function isMale(person) {
-    return person.gender === 'male';
-}
+changeColorEl.addEventListener('click', function() {
+    paragraphEl.style.color = 'blue';
+});
 
-function filter(arr, ruleFunction) {
-    return arr
-        .map(item => (ruleFunction(item) ? item : null)) 
-        .filter(item => item !== null); 
-}
+// Задание 3
 
-console.log(filter([3, -4, 1, 9], isPositive)); 
+const changeTextEl = document.querySelector('.change-text');
 
-const people2 = [
-    {name: 'Глеб', gender: 'male'},
-    {name: 'Анна', gender: 'female'},
-    {name: 'Олег', gender: 'male'},
-    {name: 'Оксана', gender: 'female'}
-];
- 
- console.log(filter(people2, isMale));
+changeTextEl.addEventListener('click', function() {
+    headingEl.textContent = 'Привет, мир!';
+});
 
- // Задание 3
+// Задание 4
 
- function logCurrentDate() {
-    const currentDate = new Date();
-    console.log(`Текущая дата и время: ${currentDate}`);
- }
+const descriprionEls = document.querySelectorAll('.description');
 
- const intervalId = setInterval(logCurrentDate, 3000);
+descriprionEls.forEach(function (element) {
+    element.textContent = 'Измененный текст';
+});
 
- setTimeout(() => {
-    clearInterval(intervalId);
-    console.log("30 секунд прошло");
- }, 30000);
+// Задание 5
 
- // Задание 4
+const newDescriptionEls = document.querySelectorAll('.new-description');
 
- function delayForSecond(callback) {
-    setTimeout(callback, 1000);
- }
+newDescriptionEls.forEach(function (el) {
+    el.textContent = 'Новый текст';
+});
 
- delayForSecond(function () {
-    console.log('Привет, Глеб!');
- });
+// Задание 6
 
- // Задание 5
+const newParagraphEl = document.querySelector('.new-paragraph');
 
- function delayForSecond2 (cb) {
-    setTimeout(() => {
-        console.log('Прошла одна секунда');
-        if(cb) {  cb(); }
-    }, 1000)
-}
+newParagraphEl.addEventListener('click', function () {
+    const newParagraph = document.createElement('p');
 
-function sayHi (name) {
-    console.log(`Привет, ${name}!`);
-}
+    newParagraph.textContent = 'Новый абзац';
+    document.body.appendChild(newParagraph);
+});
 
-delayForSecond2 (function() {
-    sayHi('Глеб');
+// Задание 7
+
+const removeEl = document.querySelector('.remove');
+
+removeEl.addEventListener('click', function () {
+    const firstDescriptionEl = document.querySelector('.description');
+
+    if (firstDescriptionEl) {
+        firstDescriptionEl.remove();
+    }
 });
